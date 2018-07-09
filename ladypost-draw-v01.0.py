8843166b73850c0e99de3cc566c02b7d7e1d112a
zipsardinia <34945360+zipsardinia@users.noreply.github.com>
2018-07-09 13:23:35 +0200

    Update ladypost-draw-v01.0.py

--- ladypost-draw-v01.0.py
+++ ladypost-draw-v01.0.py
@@ -3,8 +3,8 @@ import csv
 import time
 
 
-prize = float(raw_input ("Prize - SBD "))
-jackpot = float(raw_input ("Jackpot - SBD "))
+prize = float(input ("Prize - SBD "))
+jackpot = float(input ("Jackpot - SBD "))
 
 
 with open('lp04018.csv') as csvfile:
@@ -13,9 +13,9 @@ with open('lp04018.csv') as csvfile:
     for row in readCSV:
         date = row[0]
         dates.append(date)
-print
-print str("Prize Pool | Montepremi: ") + str(prize) + str(" SBD")
-print
+print ()
+print (str("Prize Pool | Montepremi: ") + str(prize) + str(" SBD"))
+print ()
 print ("These are the winners | I vincitori sono:")
 prize1 = round(prize * 0.50,3)
 prize2 = round(prize * 0.25,3)
@@ -31,9 +31,11 @@ while x <= 3:
         numero = random.randint(1,len(dates))
         if numero not in l:
             l.append(numero)
-
-    	    print str(x) + str(". Lucky Number N.") + str((numero)),str("@") + str(dates[numero-1] + str(" - ") + str(premi[x-1]) + str(" SBD"))
+            p=x
+            print 
+            print (str(x) + (". Lucky Number N.") + str((numero)), ("@") + (dates[numero-1]) , str(premi[p-1]))
             x=x+1
-print str("Jackpot - ") + str(jackpot)
-print
-print "DRAW TIME:", (time.strftime("%d/%m/%Y")) , (time.strftime("%H:%M:%S"))
+
+print (str("Jackpot - ") + str(jackpot))
+print ()
+print ("DRAW TIME:", (time.strftime("%d/%m/%Y")) , (time.strftime("%H:%M:%S")))
