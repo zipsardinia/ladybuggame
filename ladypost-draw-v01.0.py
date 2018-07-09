1703635ad2f1162ead918788c8010347772c2b19
zipsardinia <34945360+zipsardinia@users.noreply.github.com>
2018-07-09 11:09:58 +0200

    Update ladypost-draw-v01.0.py

--- ladypost-draw-v01.0.py
+++ ladypost-draw-v01.0.py
@@ -1,5 +1,8 @@
 # LadybugGame è un gioco di fortuna. Gira sulla blockchain di steem.
-# 
+# Ladybug Game is a game of luck. To win is simple and free.
+# Just vote. It does not matter if you are a whale or a placton, you will have the same odds.
+# Upvote now!
+# http://ladybuggame.altervista.org/
 
 import random
 import csv
